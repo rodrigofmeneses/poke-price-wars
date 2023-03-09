22fcdedcbd3a73c287a1cfad6495bd09ab34a216
Rodrigo Meneses <rodrigofmeneses97@gmail.com>
2023-03-09 09:36:29 -0300

    feat: Add basic structure to App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,36 @@
 import { useState } from "react";
 import "./app.css";
-import reactLogo from "./assets/react.svg";
 
-function App() {
-  const [count, setCount] = useState(0);
+const App = () => {
+  const [showGame, setShowGame] = useState<boolean>(false);
+  const [cards, setCards] = useState(["123"]);
+
+  const handleStartClick = () => {
+    setShowGame(true);
+  };
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
+    <main>
+      {!showGame && (
+        <>
+          <h1>Welcome to SnapWars</h1>
+          <h2>Marvel Snap guessing game</h2>
+          {cards.length === 0 ? (
+            <p>Loading Game</p>
+          ) : (
+            <button type="submit" onClick={handleStartClick}>
+              Start
+            </button>
+          )}
+        </>
+      )}
+      {showGame && (
+        <>
+          <h1>Game</h1>
+        </>
+      )}
+    </main>
   );
-}
+};
 
 export default App;
